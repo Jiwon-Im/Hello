1d963928de192393fd5b98e567ec9c11b8f52e6d
Jiwon-Im <wldnjs4255@naver.com>
2020-09-24 16:22:24 +0900

    button

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.hello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void InitializeView()
+    {
+        btn = (Button)findViewById(R.id.button);
+    }
+    public void SetListener()
+    {
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                textView.setTextColor(Color.RED);
+            }
+        });
     }
 }
